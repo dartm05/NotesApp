4cf302feab6a893dc8a6a7ef29f868051141f3bf
Daniella Arteaga <darteag@bancolombia.com.co>
2024-10-19 23:48:11 -0400

    front end setup

---
+++ front/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideAnimationsAsync('noop'), provideFirebaseApp(() => initializeApp({"projectId":"tasks-app-b53c1","appId":"1:837347397183:web:7459af4a625032b543b28a","storageBucket":"tasks-app-b53c1.appspot.com","apiKey":"AIzaSyBVKECxc5lOkJLBl_Siv7MjBY7TiwFMeu0","authDomain":"tasks-app-b53c1.firebaseapp.com","messagingSenderId":"837347397183","measurementId":"G-TYQ8Z7H17C"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+};
---
+++ front/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBVKECxc5lOkJLBl_Siv7MjBY7TiwFMeu0",
+    authDomain: "tasks-app-b53c1.firebaseapp.com",
+    projectId: "tasks-app-b53c1",
+    storageBucket: "tasks-app-b53c1.appspot.com",
+    messagingSenderId: "837347397183",
+    appId: "1:837347397183:web:7459af4a625032b543b28a",
+    measurementId: "G-TYQ8Z7H17C"
+  },
+};
