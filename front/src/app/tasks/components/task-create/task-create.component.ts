c5abf1184dd04e460c9330157f17d42b7f42b748
Daniella Arteaga <darteag@bancolombia.com.co>
2024-10-24 18:57:08 -0400

    feat(task): Front add task create modal

---
+++ front/src/app/tasks/components/task-create/task-create.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, inject, Input, Output } from "@angular/core";
+import { TasksService } from "../../services/tasks.service";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+  selector: "app-task-create",
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: "./task-create.component.html",
+  styleUrl: "./task-create.component.css",
+})
+export class TaskCreateComponent {
+  constructor(private tasksService: TasksService) {}
+
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  enteredTitle = "";
+  description = "";
+  enteredDate = "";
+
+  onCancel() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    const newTask = {
+      id: "",
+      title: this.enteredTitle,
+      description: this.description,
+      createdAt: new Date(),
+      done: false,
+    };
+    this.tasksService.createTask(newTask).subscribe((createdTask) => {
+      this.tasksService.tasks.set([...this.tasksService.tasks(), createdTask]);
+      this.close.emit();
+    });
+  }
+}
