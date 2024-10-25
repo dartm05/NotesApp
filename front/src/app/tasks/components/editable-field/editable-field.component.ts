c67e13a7940609a22ffdf014271a15705ad6bbde
Daniella Arteaga <darteag@bancolombia.com.co>
2024-10-25 06:20:03 -0400

    feat(front): Add EditModeDirective and ViewModeDirective for editable fields

---
+++ front/src/app/tasks/components/editable-field/editable-field.component.ts
@@ -0,0 +1,54 @@
+import {
+  Component,
+  ContentChild,
+  effect,
+  ElementRef,
+  input,
+  output,
+  signal,
+  TemplateRef,
+} from "@angular/core";
+
+import { ViewModeDirective } from "./directives/view-mode.directive";
+import { EditModeDirective } from "./directives/edit-mode.directive";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-editable-field",
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <ng-container *ngIf="isEditMode(); else viewTemplate">
+      <ng-container *ngTemplateOutlet="editTemplate"></ng-container>
+    </ng-container>
+    <ng-template #viewTemplate>
+      <ng-container *ngTemplateOutlet="viewTemplateRef"></ng-container>
+    </ng-template>
+  `,
+  styleUrl: "./editable-field.component.css",
+})
+export class EditableFieldComponent {
+  isEditMode = input<boolean>();
+
+  @ContentChild("viewMode", { static: true })
+  viewTemplateRef!: TemplateRef<any>;
+  @ContentChild("editMode", { static: true }) editTemplate!: TemplateRef<any>;
+
+  mode: "view" | "edit" = "view";
+
+  _editMode = signal<boolean>(false);
+  editMode = this._editMode.asReadonly();
+
+  get currentReference() {
+    console.log(this.mode);
+    return this.mode === "view" ? this.viewTemplateRef : this.editTemplate;
+  }
+
+  constructor() {
+    effect(() => {
+      this.mode = this.isEditMode() ? "edit" : "view";
+    });
+  }
+
+  ngOnDestroy() {}
+}
