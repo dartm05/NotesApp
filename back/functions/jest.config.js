4dac1553a2ce0db4660c267a711cdbcce2bfc421
Daniella Arteaga <darteag@bancolombia.com.co>
2024-10-27 03:20:31 -0400

    tests(setup): Back add test config with jest and added domain tests, infrastructure

--- back/functions/jest.config.js
+++ back/functions/jest.config.js
@@ -1,8 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-    "\\.js$": " /Users/daniellaarteaga/Documents/Projects/TasksApp/back/functions/node_modules/babel-jest" 
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
